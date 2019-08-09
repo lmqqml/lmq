5df6b101accab0848c24b8b37b1fcc3f1f32cba5
lmqqml <815003393@qq.com>
2019-08-09 20:39:16 +0800

    list提交的代码

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("提交新代码到服务器");
+		System.out.println("list提交新代码到服务器");
     }
 }
