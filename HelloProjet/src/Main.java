52e23665fb25bb46d0ff6760313240d228f3880d
lmqqml <815003393@qq.com>
2019-08-09 21:00:07 +0800

    拼写错误修改1

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 public class Main {
 
     @Test
-    public void testHelloWrold() {
+    public void testHelloWorld() {
         System.out.println("HelloWorld!");
         System.out.println("提交新代码到服务器");
 		System.out.println("list提交新代码到服务器");
